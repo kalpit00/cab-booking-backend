c83788a21a5dc046f7ed624f55ebf454992cd844
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-10-12 06:25:29 +0530

    Add a Put mapping to setup a updateLocation method from service. use Kafka here to publish

--- cab-book-driver/src/main/java/com/kalpit00/cab_book_driver/service/CabLocationService.java
+++ cab-book-driver/src/main/java/com/kalpit00/cab_book_driver/service/CabLocationService.java
@@ -1,4 +1,12 @@
 package com.kalpit00.cab_book_driver.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CabLocationService {
+    public boolean updateLocation(String location) {
+        System.out.println("Send coordinate using Kafka here");
+        return true;
+    }
+
 }
